69260fdd5ec869cfbb271f44ea575b5456331547
Bhawana <bhawanakochar692.hitcse2020@gmail.com>
2026-08-01 21:44:19 +0530

    Add common library shared components

---
+++ common-library/src/main/java/com/bhawana/commonlibrary/constants/AppConstants.java
@@ -0,0 +1,14 @@
+package com.bhawana.commonlibrary.constants;
+
+public class AppConstants {
+
+    public static final String EMPLOYEE_CREATED =
+            "Employee created successfully";
+
+    public static final String EMPLOYEE_UPDATED =
+            "Employee updated successfully";
+
+    public static final String EMPLOYEE_DELETED =
+            "Employee deleted successfully";
+
+}
---
+++ common-library/src/main/java/com/bhawana/commonlibrary/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.bhawana.commonlibrary.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+}
